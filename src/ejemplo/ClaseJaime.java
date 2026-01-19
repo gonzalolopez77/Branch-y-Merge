36d83c6e1f70a26b8659712ab1695fc95df9ce44
gonzalolopez77 <gonzalo.lopez@iesnervion.es>
2026-01-19 12:15:25 +0100

    push gonzalo

--- src/ejemplo/ClaseJaime.java
+++ src/ejemplo/ClaseJaime.java
@@ -5,8 +5,12 @@ public class ClaseJaime {
 	public static void main(String[] args) {
 		String mensaje = "Hola, soy Jaime";
 		System.out.println(mensaje);
-		int numero = 10;
+		int numero = 15;
 		System.out.println("El número es: " + numero);
+		
+		System.out.println("Gonzalo");
+		numero = 20;
+		System.out.println("El numero es: " + numero);
 
 	}
 
