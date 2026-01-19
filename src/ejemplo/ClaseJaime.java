dc05ad25f25b3044b4ee0cff9107ff664b999530
jaime98-coder <jaime.martin@iesnervion.es>
2026-01-19 12:17:15 +0100

    subida jaimeee

--- src/ejemplo/ClaseJaime.java
+++ src/ejemplo/ClaseJaime.java
@@ -4,7 +4,9 @@ public class ClaseJaime {
 
 	public static void main(String[] args) {
 
-
+		System.out.println("Hola, soy Jaime!");
+		int numero = 42;
+		System.out.println("El número favorito de Jaime es: "+numero);
 	}
 
 }
