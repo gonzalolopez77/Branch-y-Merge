da205a2f478b8e416fbd50b1e39f9d41a984dab7
gonzalolopez77 <gonzalo.lopez@iesnervion.es>
2026-01-19 12:15:53 +0100

    .

--- src/ejemplo/ClaseJaime.java
+++ src/ejemplo/ClaseJaime.java
@@ -3,14 +3,7 @@ package ejemplo;
 public class ClaseJaime {
 
 	public static void main(String[] args) {
-		String mensaje = "Hola, soy Jaime";
-		System.out.println(mensaje);
-		int numero = 15;
-		System.out.println("El número es: " + numero);
-		
-		System.out.println("Gonzalo");
-		numero = 20;
-		System.out.println("El numero es: " + numero);
+
 
 	}
 
