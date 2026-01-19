8e0da77cac1db4950d492f4a59b1855168e76abf
jaime98-coder <jaime.martin@iesnervion.es>
2026-01-19 11:58:09 +0100

    Conflicto resuelto manualmente

---
+++ git/Branch-y-Merge/src/ejemplo/ClaseJaime.java
@@ -0,0 +1,11 @@
+package ejemplo;
+
+public class ClaseJaime {
+
+	public static void main(String[] args) {
+		String mensaje = "Hola, soy Jaime";
+		System.out.println(mensaje);
+
+	}
+
+}
---
+++ git/Branch-y-Merge/src/ejemplo/Ejemplo1.java
@@ -0,0 +1,12 @@
+package ejemplo;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Rama de Gonzalo");
+		
+	}
+
+}
